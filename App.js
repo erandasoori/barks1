3c10f1a91b4b17918bb446152db34c6f4a0a837d
Eranda Sooriyarachchi <elsoori@yahoo.com>
2023-09-21 14:23:37 -0500

    testing

--- App.js
+++ App.js
@@ -1,22 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
+import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
-import tailwind from 'tailwind-rn'
+import { StyleSheet, Text, View } from "react-native";
+import tw from "tailwind-rn";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This is Eranda's first app!</Text>
+    <View style={tw("flex-1 justify-center items-center")}>
+      <Text>Hello PAPPAFAM 123</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
